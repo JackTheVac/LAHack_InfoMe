3c72e89d3e22ce502574cd7d2771099e94da61ce
rickisXD <rickyyu030102@gmail.com>
2021-03-23 01:36:03 -0700

    moved firebase config to app.js

--- App.js
+++ App.js
@@ -9,6 +9,21 @@ import Name from './src/components/name/name'
 import Occupation from './src/components/occupation/occupation'
 import AddInfo from './src/components/addInfo/addInfo'
 
+import * as firebase from 'firebase';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWFzFXweu4VL9uImTO-OQQTtoVvPfg9rA",
+  authDomain: "infome-31dd7.firebaseapp.com",
+  databaseURL: "https://infome-31dd7-default-rtdb.firebaseio.com/",
+  projectId: "infome-31dd7",
+  storageBucket: "infome-31dd7.appspot.com",
+  messagingSenderId: "266415670678",
+  appId: "1:266415670678:web:7f3410360a27f232b249b7",
+  measurementId: "G-TYLD1V2QHR"
+};
+firebase.initializeApp(firebaseConfig);
+
 export default function App() {
   return (
     
--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -45,6 +45,7 @@ export default class LoginForm extends Component {
 const styles = StyleSheet.create({
     container: {
         padding: 20,
+        flex: 1
     },
     input: {
         height: 40,
