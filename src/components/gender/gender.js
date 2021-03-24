56f89306e42373e2c5997553ded25e92335f3087
ssunnlee <california.sunlee@gmail.com>
2021-03-23 22:56:45 -0700

    Gender to Age Navigation

--- src/components/gender/gender.js
+++ src/components/gender/gender.js
@@ -1,15 +1,36 @@
 import { styleSheets } from 'min-document';
 import React, { Component} from 'react';
-import {StyleSheet, View, Image, Text, KeyboardAvoidingView} from 'react-native';
+import {StyleSheet, View, Image, Text, KeyboardAvoidingView, Button} from 'react-native';
 import GenderForm from './genderForm';
 import '../../config/global.js'
 
 export default class Gender extends Component {
+    constructor(props){
+        super(props)
+        this.state = ({
+            gender:''
+        })
+    }
+
+    recordGender = (gender) => {
+        //NOTE this adds to firebase data collection for thegamerguy321@gmail.com
+        userdata.collection("bk51SvrRN7eFslolmPi0Ck5IlCy1").doc("userInfo").set({
+            genderID: gender
+        });
+        this.props.navigation.navigate('Age');
+    }
+
     render(){
         return (
             <KeyboardAvoidingView behavior = "padding" style = {styles.container}>
                 <View style = {styles.formContainer}>
                     <GenderForm />
+                    <Button //touchable opacity styling?
+                    style = {styles.buttonContainer}
+                    title = "Confirm and Proceed"
+                    color = "#3C6435"
+                    onPress={()=> this.recordGender(this.state.gender)}
+                />
                 </View>
             </KeyboardAvoidingView>
         );
@@ -34,5 +55,16 @@ const styles = StyleSheet.create({
         marginTop: 10,
         textAlign: 'center',
         opacity: 0.5,
-    }
+    },
+    buttonContainer: {
+        backgroundColor: "#3C6435",
+        // position: 'absolute',
+        // margin: 16,
+        // right: 10,
+        // bottom: 10,
+        // //left: 0,
+        // //marginTop: '170%',
+        paddingVertical: 15
+        // //marginBottom: '10%'
+    },
 });
